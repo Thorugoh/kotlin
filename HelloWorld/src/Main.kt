05af39fbe63f481fc0677647a05cf130b1cfc084
Victor Hugo <pavictorhugo@gmail.com>
2024-12-21 18:21:51 -0300

    hello world

--- HelloWorld/src/Main.kt
+++ HelloWorld/src/Main.kt
@@ -1,17 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    println("Enter your name: ")
-    val name = readln()
-
-    println("Hello $name")
-
-    println("Enter your age: ")
-    val age = readln().toInt()
-
-    for(i in 1..age) {
-        val ageText = if (i == age) "Your age is $i!" else "$i..."
-        println(ageText)
-    }
+    val result = Math.sqrt(16.0)
+    println(result)
 
 }
